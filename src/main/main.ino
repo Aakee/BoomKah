fe768483560f833ae9423b40b41b640ac658ae29
Akseli Konttas <aakeli.kaakeli@gmail.com>
2024-06-19 00:49:08 +0300

    Modified main.ino so that it supports the new handlers

--- src/main/main.ino
+++ src/main/main.ino
@@ -7,6 +7,7 @@
 
     Author: Akseli Konttas 2024
 */
+
 #include "switch_handler.h"
 #include "module_led_handler.h"
 #include "rgb_handler.h"
@@ -22,32 +23,38 @@
 // Define pins for actuators
 
 // Module LEDs
-const int RED_LED_1   = 1;
-const int RED_LED_2   = 3;
-const int RED_LED_3   = 5;
+const int MOD_LED_1   = 0;
+const int MOD_LED_2   = 1;
+const int MOD_LED_3   = 2;
+const int MOD_LED_4   = 3;
 
 // Blinker LEDs
-const int WHT_LED    = 6;
-const int BLU_LED    = 7;
-const int YLW_LED    = 8;
-
-// RGB LED
-const int RGB_RED   = 9;
-const int RGB_BLUE  = 10;
-const int RGB_GREEN = 11;
-
-// Buttons
-const int BTN1 = 0;
-const int BTN2 = 2;
-const int BTN3 = 4;
-const int BTN4 = 12;
+const int RED_LED    = 4;
+const int GRN_LED    = 5;
+const int BLU_LED    = 6;
+const int YLW_LED    = 7;
 
 // Buzzer
-const int BUZ = 13;
+const int BUZ        = 8;
 
-// Potentiometers
-const int POT1 = A0;
-const int POT2 = A1;
+// RGB LED
+const int RGB_RED    = 9;
+const int RGB_BLUE   = 10;
+const int RGB_GREEN  = 11;
+
+// RG LED
+const int RG_RED     = 12;
+const int RG_GREEN   = 13;
+
+// Buttons
+const int BTN1       = A0;
+const int BTN2       = A1;
+const int BTN3       = A2;
+const int BTN4       = A3;
+
+// Switches
+const int SWTCH1     = A4;
+const int SWTCH2     = A5;
 
 
 // Setup function (nothing here at the moment)
@@ -62,67 +69,43 @@ void loop() {
   // Define static objects (same for each loop)
 
   // Event and hardware managers
-  static const auto switches         = SwitchHandler(BTN1, BTN2, BTN3, BTN4, POT1, POT2);
-  static const auto moduleLeds       = ModuleLedHandler(RED_LED_1, RED_LED_2, RED_LED_3);
+  static const auto switches         = SwitchHandler(BTN1, BTN2, BTN3, BTN4, SWTCH1, SWTCH2);
+  static const auto moduleLeds       = ModuleLedHandler(MOD_LED_1, MOD_LED_2, MOD_LED_3, MOD_LED_4);
   static const auto rgb              = RGBHandler(RGB_RED, RGB_GREEN, RGB_BLUE);
-  static const auto blinkers         = BlinkerHandler(WHT_LED, BLU_LED, YLW_LED);
-  static const auto feedback         = FeedbackHandler(BUZ);
+  static const auto blinkers         = BlinkerHandler(RED_LED, GRN_LED, BLU_LED, YLW_LED);
+  static const auto feedback         = FeedbackHandler(BUZ, RG_RED, RG_GREEN);
 
   // Modules, i.e. tasks for the players.
-  static const auto firstTask        = ButtonModule();
-  static const auto secondTask       = DirectionsModule();
-  static const auto thirdTask        = MemoryModule();
+  //static const auto firstTask        = ButtonModule();
+  //static const auto secondTask       = DirectionsModule();
+  //static const auto thirdTask        = MemoryModule();
   
 
   // Read and get button and potentiometer states
   switches.read();
   SwitchStates* states = switches.get();
 
-  // Initialize return values for the modules
-  int ret = 0;  bool moduleCompleted = false;
 
+  bool canContinue = feedback.tick();
+  if (!canContinue) {return;}
 
-  // Idea: Find the first module which isn't still completed. Set its module led on.
-  // Call the "run" method on the said module, and collect the return value and the status whether the
-  // module has now been deactivated or not.
-  // Return value of the "run" methods: 1 if user did something right, -1 if wrong, and 0 otherwise.
+  char ledValue = 0;
+  if (states->btn1) {ledValue += 0b0001;}
+  if (states->btn2) {ledValue += 0b0010;}
+  if (states->btn3) {ledValue += 0b0100;}
+  if (states->btn4) {ledValue += 0b1000;}
+  blinkers.set(ledValue);
 
-  // First task
-  if (!firstTask.isCompleted()) {
-    moduleLeds.set(1);
-    ret = firstTask.run(states, &rgb, &blinkers);
-    moduleCompleted = firstTask.isCompleted();
-  }
+  int redval = 0;  int bluval=0;  int grnval = 0;
+  if (states->swtch1 < 0) {redval = 10;}
+  if (states->swtch1 > 0) {bluval = 10;}
+  if (states->swtch2 < 0) {redval = 10;}
+  if (states->swtch2 > 0) {grnval = 10;}
+  rgb.set(redval, grnval, bluval);
 
-  // Second task
-  else if (!secondTask.isCompleted()) {
-    moduleLeds.set(2);
-    ret = secondTask.run(states, &rgb, &blinkers);
-    moduleCompleted = secondTask.isCompleted();
-  }
-
-  // Third task
-  else if (!thirdTask.isCompleted()) {
-    moduleLeds.set(3);
-    ret = thirdTask.run(states, &rgb, &blinkers);
-    moduleCompleted = thirdTask.isCompleted();
-  }
-
-  // If all tasks are done, simply turn off all module leds, blinkers and the RGB led
-  else {
-    moduleLeds.off();
-    rgb.off();
-    blinkers.off();
-  }
-
-  // Check return value of the module and activate feedback
-  if      (ret ==  1 && !moduleCompleted) {feedback.single_correct();}
-  else if (ret ==  1 && moduleCompleted && !thirdTask.isCompleted()) {feedback.double_correct();}
-  else if (ret ==  1 && thirdTask.isCompleted()) {feedback.triple_correct();}
-  else if (ret == -1) {feedback.error();}
-
-  // Tick the feedback, i.e. check if a new sound needs to be played
-  feedback.tick();
-  //delay(100);
+  if (states->btn1) {feedback.correct();}
+  if (states->btn2) {feedback.module_deactivated();}
+  if (states->btn3) {feedback.bomb_defused();}
+  if (states->btn4) {feedback.error();}
 
 }
